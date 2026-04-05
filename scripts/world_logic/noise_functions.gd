ceb5fdf256ca5c605490aba78c2ebb035f4cfba4
Torsten Gustafsson <torstengustafsson1992@gmail.com>
2026-04-05 10:30:34 +0200

    Update models and noise functions
    Update models to look better on steep terrain.
    Add noise subtractive noise, so there are areas with no forest or rocks.

--- scripts/world_logic/noise_functions.gd
+++ scripts/world_logic/noise_functions.gd
@@ -2,20 +2,31 @@ class_name NoiseFunctions
 
 class NoiseContainer:
 	var noise: FastNoiseLite
-	var threshold: float # Value between 0.0 and 1.0, 0.4 means keep 40% of the area, 0.25 means keep 25% of the area
+	var threshold: float # Value between 0.0 and 1.0, 0.4 means keep ~40% of the area, 0.25 means keep ~25% of the area
+	var additive = true
 
 var noises: Array[NoiseContainer] = []
 
 func _init(noises_array: Array[NoiseContainer]) -> void:
 	noises = noises_array
 
+func get_num_additive_noises() -> int:
+	var result = 0
+	for noise in noises:
+		result += int(noise.additive)
+	return result
+
 func above_threshold(position: Vector3) -> bool:
 	var num_true: int = 0
+	var num_needed = get_num_additive_noises()
 	for noise_container in noises:
 		var noise_value = (noise_container.noise.get_noise_2d(position.x, position.z) + 1) / 2.0
 		if noise_value > noise_container.threshold:
-			num_true += 1
-	return num_true == noises.size()
+			if noise_container.additive:
+				num_true += 1
+			else:
+				return true # Non-additive noise cuts directly
+	return num_true == num_needed
 
 static func create_forest_noise() -> NoiseFunctions:
 	var high_density_noise = FastNoiseLite.new()
@@ -30,13 +41,27 @@ static func create_forest_noise() -> NoiseFunctions:
 	noise_container_2.noise = low_density_noise
 	noise_container_2.threshold = 0.25
 
-	return NoiseFunctions.new([noise_container_1, noise_container_2])
+	var no_forest_noise = FastNoiseLite.new()
+	no_forest_noise.frequency = 0.0006
+	var noise_container_3 = NoiseContainer.new()
+	noise_container_3.noise = no_forest_noise
+	noise_container_3.threshold = 0.6
+	noise_container_3.additive = false
+
+	return NoiseFunctions.new([noise_container_1, noise_container_2, noise_container_3])
 
 static func create_rocks_noise() -> NoiseFunctions:
 	var noise = FastNoiseLite.new()
 	noise.frequency = 0.8
-	var noise_container = NoiseContainer.new()
-	noise_container.noise = noise
-	noise_container.threshold = 0.3
+	var noise_container_1 = NoiseContainer.new()
+	noise_container_1.noise = noise
+	noise_container_1.threshold = 0.3
 
-	return NoiseFunctions.new([noise_container])
+	var no_rocks_noise = FastNoiseLite.new()
+	no_rocks_noise.frequency = 0.001
+	var noise_container_2 = NoiseContainer.new()
+	noise_container_2.noise = no_rocks_noise
+	noise_container_2.threshold = 0.6
+	noise_container_2.additive = false
+
+	return NoiseFunctions.new([noise_container_1, noise_container_2])
